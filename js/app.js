ca2140f1002010bfe3faf2c157917c4221c6ce92
LaneBrents <lanebrents@gmail.com>
2022-07-22 13:19:41 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/*----- constants -----*/
+// the board squares
+// current player / names
+// reset button
+// the winner
+
+
+
+/*----- app's state (variables) -----*/
+// the location of where each player puts their chip
+// listens for the winner and changes the score accordingly
+
+
+
+
+/*----- cached element references -----*/
+// player score
+// button
+
+
+
+
+
+/*----- event listeners -----*/
+// On click, puts the chip in the correct location
+
+
+
+
+
+/*----- functions -----*/
+// init(), correctly set the webpage to its starting state
+
+
